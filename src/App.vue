bf959ea6d5fc1f025c57b46f75998bc5411d2b3d
Oluwafemi <olukoyaoluwafemi14@gmail.com>
2022-05-05 22:26:50 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <main-sidebar />
+    <faq-section />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainSidebar from "@/components/Sidebar.vue";
+import FaqSection from "./components/FaqSection.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    "main-sidebar": MainSidebar,
+    FaqSection,
+  },
+};
 </script>
 
 <style>
+@font-face {
+  font-family: Metropolis;
+  src: url(@/assets/fonts/Metropolis-Regular.otf);
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: Metropolis;
+  src: url(@/assets/fonts/Metropolis-SemiBold.otf);
+  font-weight: 500;
+}
+
+@font-face {
+  font-family: Metropolis;
+  src: url(@/assets/fonts/Metropolis-Bold.otf);
+  font-weight: 700;
+}
+
+:root {
+  --purple: #391d49;
+  --yellow: #ffbd69;
+  --white: #fcfcfc;
+  --black: #121212;
+}
+
+/* CSS RESET */
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: Metropolis, sans-serif;
+}
+
+html {
+  font-size: 10px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100vw;
+}
+
+main {
+  height: 100%;
+  width: 100%;
+  display: flex;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+li {
+  list-style-type: none;
+}
+
+button {
+  cursor: pointer;
 }
 </style>
---
+++ src/components/FaqSection.vue
@@ -0,0 +1,113 @@
+<template>
+  <section class="middle">
+    <div class="heading">
+      <h2>FAQs</h2>
+      <p>How can we help you?</p>
+    </div>
+    <form>
+      <input
+        type="text"
+        class="form-input"
+        placeholder="Please enter your question"
+      />
+      <input class="form-btn" type="submit" value="Send" />
+    </form>
+    <!-- <section class="questions">
+      <article>
+        <div class="question-header">
+          <h4>
+            <i class="fa-solid fa-arrow-right"></i>
+            How many pictures am I allowed to upload?
+          </h4>
+          <p>
+            There is no limit to the number of images you can upload on your
+            account, as number depends on the owner of the account.
+          </p>
+        </div>
+      </article>
+
+      <div class="confirmation">
+        <p>Was this article helpful?</p>
+        <div class="confirmation-container">
+          <button>Yes</button>
+          <button>No</button>
+        </div>
+        <button>Help Center</button>
+      </div>
+    </section> -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: "FaqSection",
+};
+</script>
+
+<style scoped>
+.middle {
+  flex: 1;
+  height: 100%;
+  padding: 4rem 8rem 4rem 2.5rem;
+  background-color: #fcfcfc;
+}
+
+.heading {
+}
+
+.heading h2 {
+  font-size: 3.2rem;
+}
+
+.heading h2 i {
+  margin-right: 1.2rem;
+}
+
+.heading p {
+  font-size: 1.8rem;
+  color: var(--black);
+  opacity: 0.7;
+  font-weight: 400;
+  margin-block: 2.2rem 1.4rem;
+}
+
+form {
+  background-color: #eeeeee;
+  position: relative;
+  margin-bottom: 2.5rem;
+  margin-right: 1.7rem;
+  border-radius: 8px;
+  display: flex;
+  height: 58px;
+}
+
+form .form-input {
+  background-color: transparent;
+  width: 100%;
+  font-size: 1.6rem;
+  padding: 29px 40px;
+  border-radius: 8px;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+  border: none;
+  outline: none;
+}
+
+form .form-input::placeholder {
+  color: #525252;
+  opacity: 0.5;
+  font-weight: 400;
+  font-size: 1.4rem;
+}
+
+form .form-btn {
+  background-color: var(--purple);
+  color: white;
+  font-size: 1.6rem;
+  border-radius: 8px;
+  height: 100%;
+  padding-inline: 7.5rem;
+  border: none;
+  cursor: pointer;
+}
+</style>
